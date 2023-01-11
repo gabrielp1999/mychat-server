778907db00d4baa8140fd4af93b58acaa8dd4f1c
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2023-01-11 20:49:04 -0300

    subindo api

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const PORT = process.env.PORT || 3001;
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: true,
+    methods: ["GET", "POST"],
+  },
+});
+
+
+app.get("/", (req, res) => {
+    res.send({
+        message: 'api ok'
+    })
+})
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+
+server.listen(PORT, () => {
+  console.log("SERVER RUNNING", PORT);
+});
