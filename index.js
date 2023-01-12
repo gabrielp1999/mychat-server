a79571919309547329605908d5961d61af676902
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2023-01-11 21:37:22 -0300

    ˜fix˜

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ app.use(cors());
 
 const server = http.createServer(app);
 
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 const io = new Server(server, {
   cors: {
     origin: true,
-    methods: ["GET", "POST"],
+    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
   },
 });
 
